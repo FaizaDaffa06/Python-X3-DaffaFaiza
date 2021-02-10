18dd610a6f1f96399b4f35bf2f03729fd6f0b877
FaizaDaffa06 <78843214+FaizaDaffa06@users.noreply.github.com>
2021-02-10 14:36:47 +0700

    Add files via upload

---
+++ Program 10.py
@@ -0,0 +1,6 @@
+Celcius=float(input("Ketik Nilai Celcius = "))
+Reamur = 4/5*Celcius
+Fahrenheit = 9/5*Celcius+32
+print("Celcius =",Celcius)
+print("Reamur  =",Reamur)
+print("Fahrenheit  =",Fahrenheit)
---
+++ Program 7.py
@@ -0,0 +1,5 @@
+num = int(input("input nilai : "))
+if (num % 2) == 0:
+    print ("{0} adalah genap".format(num))
+else:
+    print ("{0} adalah ganjil".format(num))
